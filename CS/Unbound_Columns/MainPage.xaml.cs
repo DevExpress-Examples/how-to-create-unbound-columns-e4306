5afcd8b7df810997a9e72cf9b7b0ab904bf2208b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Unbound_Columns/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DevExpress.Data;
+using Windows.UI.Xaml.Controls;
+
+
+namespace Unbound_Columns
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            grid.ItemsSource = new ProductList();
+            grid.TotalSummary.Add(SummaryItemType.Count, "ProductName");
+            grid.TotalSummary.Add(SummaryItemType.Max, "UnitPrice").DisplayFormat = "Max: {0:c2}";
+            grid.TotalSummary.Add(SummaryItemType.Average, "UnitPrice").DisplayFormat = "Avg: {0:c2}";
+        }
+
+        private void grid_CustomUnboundColumnData(object sender, DevExpress.UI.Xaml.Grid.GridColumnDataEventArgs e) {
+            if (e.IsGetData) {
+                int price = Convert.ToInt32(e.GetListSourceFieldValue("UnitPrice"));
+                int quantity = Convert.ToInt32(e.GetListSourceFieldValue("Quantity"));
+                e.Value = price * quantity;
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to: Create Unbound Columns
+
+
+<p>This example shows how to add an unbound column to the Grid Control.</p><p><br />
+</p>
+
+<br/>
+
+
